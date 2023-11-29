e0615b3bbed819cbd970007ae053acf349290116
Steven Reddy <steveng.reddy@gmail.com>
2023-11-29 15:53:11 +0000

    React-Todo

---
+++ src/TodoForm.jsx
@@ -0,0 +1,46 @@
+import ListItem from "@mui/material/ListItem"
+import TextField from "@mui/material/TextField"
+import CreateIcon from '@mui/icons-material/Create';
+import { InputAdornment } from "@mui/material";
+import { IconButton } from "@mui/material";
+import { useState } from "react";
+
+export default function TodoForm({ addTodo }) {
+    const [text, setText] = useState("");
+
+    const handleChange = (evt) => {
+        setText(evt.target.value)
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTodo(text);
+        setText("");
+    }
+    return (
+
+        <ListItem>
+            <form onSubmit={handleSubmit}>
+                <TextField id="outlined-basic"
+                    label="New Todo"
+                    variant="outlined"
+                    onChange={handleChange}
+                    value={text}
+                    InputProps={{
+                        endAdornment: (
+                            < InputAdornment position="end" >
+                                <IconButton
+                                    aria-label="create todo"
+                                    edge="end"
+                                    type="submit"
+                                >
+                                    <CreateIcon />
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+            </form >
+        </ListItem >
+    )
+}
